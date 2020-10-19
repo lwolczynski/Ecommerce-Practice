2a9e805c277ee1ad883cd3d2b4fd875b0451f161
unknown <lwolczynski7@gmail.com>
2020-10-18 21:23:09 -0500

    [refactor] Add layout template for auth sites

--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,13 +1,17 @@
+const layout = require('../layout');
+
 module.exports = ({ req }) => {
-    return `
-        <div>
-            Your ID: ${req.session.userId}
-            <form method="POST">
-                <input name='email' placeholder='email'></input>
-                <input name='password' placeholder='password'></input>
-                <input name='passwordConfirmation' placeholder='password confirmation'></input>
-                <button>Sign Up</button>
-            </form>
-        </div>
-    `
+    return layout({
+        content: `
+            <div>
+                Your ID: ${req.session.userId}
+                <form method="POST">
+                    <input name='email' placeholder='email'></input>
+                    <input name='password' placeholder='password'></input>
+                    <input name='passwordConfirmation' placeholder='password confirmation'></input>
+                    <button>Sign Up</button>
+                </form>
+            </div>
+        `
+    });
 }
